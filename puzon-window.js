9c64380ef44f4e724d96b9468f8336c1b26c1b38
Harry Willis <harry.willis@king.com>
2015-09-09 13:11:09 +0100

    Added idle timing. Added mousemove and keypress events

--- puzon-window.js
+++ puzon-window.js
@@ -9,6 +9,7 @@ var Puzon = function(opts) {
 _.extend(Puzon.prototype, Backbone.Events, {
   initialize: function(options){
     this.element = $(options.element);
+    this.idleTime = options.idleTime;
     this.clock = 0;
     this.lessonTimer = 0;
     this.start();
@@ -17,7 +18,12 @@ _.extend(Puzon.prototype, Backbone.Events, {
   tick: function() {
     this.clock++;
     this.lessonTimer++;
-    console.log(this.clock, this.lessonTimer);
+    if(this.clock > this.idleTime) {
+      console.log('Idle time reached, pausing lesson');
+      this.trigger('lesson_paused')
+      this.pause();
+    }
+    console.log(this.clock, this.lessonTimer, this.idleTime);
   },
   reset: function() {
     console.log('reset called');
@@ -39,6 +45,8 @@ _.extend(Puzon.prototype, Backbone.Events, {
     this.element.get(0).addEventListener("scroll", _.bind(this.reset, this), true);
     this.element.get(0).addEventListener("blur", _.bind(this.pause, this), true);
     this.element.get(0).addEventListener("focus", _.bind(this.start, this), true);
+    this.element.get(0).addEventListener("mousemove", _.bind(this.reset, this), true);
+    this.element.get(0).addEventListener("keypress", _.bind(this.reset, this), true);
   }
 })
 module.exports = Puzon;
